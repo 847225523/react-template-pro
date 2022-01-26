22f54ba2631e511286f103121bb113c5c6d70a38
zhw <847225523@qq.com>
2022-01-26 15:46:25 +0800

    feat(git): add 增加提交信息校验

--- scripts/verify-commit-msg.js
+++ scripts/verify-commit-msg.js
@@ -1,7 +1,7 @@
 const msg = require('fs').readFileSync('.git/COMMIT_EDITMSG', 'utf-8').trim()
+const chalk = require('chalk')
 
 const commitRE = /^(revert: )?(feat|fix|polish|docs|style|refactor|perf|test|workflow|ci|chore|types|build)(\(.+\))?: .{1,50}/
-
 if (!commitRE.test(msg)) {
     console.log()
     console.error(
